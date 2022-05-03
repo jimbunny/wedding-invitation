6e5c3b899fc14495f6dcff515688a0237e97346b
jimbunny <954447255@qq.com>
2022-05-03 17:30:58 +0700

    demo

--- api/static/website/assets/js/main.js
+++ api/static/website/assets/js/main.js
@@ -3,11 +3,11 @@
 
   // Spinner
   var spinner = function () {
-      setTimeout(function () {
-          if ($('#spinner').length > 0) {
-              $('#spinner').removeClass('show');
-          }
-      }, 1);
+    setTimeout(function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
+    }, 1);
   };
   spinner();
   
