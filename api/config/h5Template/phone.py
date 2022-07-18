95d4ecbbc015b3f4a13df5e63c538704df96a15f
jimbunny <954447255@qq.com>
2022-07-18 17:36:53 +0700

    demo

--- api/config/h5Template/phone.py
+++ api/config/h5Template/phone.py
@@ -150,8 +150,12 @@ def phone(url, name):
                 contentBg.style.right =  (parseFloat(getComputedStyle(dom1).width.substring(0,7))/2*3+18).toString() + "px"
             }
             $('#talkbubble').css('display', 'block')
+            $('.maka-Arrow').css('display', 'none !important')
             $('div').css('font-family', ' ''' + str(font.get('name')) + ''' ', ' ''' + str(font.get('family')) + ''' !important')
         }, 2000);
+        setTimeout(function () {
+            $('.maka-Arrow').css('display', 'none !important')
+        }, 1);
         setTimeout(function () {
             $('div').css('font-family', ' ''' + str(font.get('name')) + ''' ', ' ''' + str(font.get('family')) + ''' !important')
         }, 5000);
